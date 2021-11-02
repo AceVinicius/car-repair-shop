0b1807e57166b40a123059f888ceeff3227a3222
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2021-11-01 21:08:49 -0300

    Fixed duplicated entries

--- src/Controller/CityController.java
+++ src/Controller/CityController.java
@@ -1,64 +1,95 @@
 package Controller;
 
-import java.util.ArrayList;
+import java.io.Serializable;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.swing.table.DefaultTableModel;
 
 import Model.City;
+import View.CrudException;
 
-public class CityController {
+public class CityController implements Serializable {
 
     /********************
      * Class Properties *
      ********************/
 
-    private static ArrayList<City> cities = new ArrayList<City>();
+    private static final long serialVersionUID = 4419346585471927334L;
+
+    private Map<String, City> cities;
 
     /**********************
      * Class Constructors *
      **********************/
 
-    public static boolean create(final String name, final String state) {
-        City newCity = new City(name, state);
-        cities.add(newCity);
-
-        return true;
+    public CityController() {
+        cities = new TreeMap<>();
     }
 
-    public static Object[] read(final int index) {
-        City city = cities.get(index);
+    /***********************
+     * Getters and Setters *
+     ***********************/
+
+    public City getCity(final Object id) {
+        return cities.get((String) id);
+    }
+
+    public Map<String, City> getAll() {
+        return cities;
+    }
+
+    public DefaultTableModel getTableModel() {
+        Object[] header = { "Name", "State" };
+
+        DefaultTableModel model = new DefaultTableModel(header, 0);
+
+        for (var entry : cities.entrySet()) {
+            Object[] row = { entry.getValue().getName(), entry.getValue().getState() };
+
+            model.addRow(row);
+        }
+
+        return model;
+    }
+
+    /******************************
+     * Additional Private Methods *
+     ******************************/
+
+    private void persist() {
+        Controller.writeFile();
+    }
+
+    /*****************************
+     * Additional Public Methods *
+     *****************************/
+
+    public void create(final String name, final String state) throws CrudException {
+        if (cities.get(name) != null) {
+            throw new CrudException("City already exists.");
+        }
+
+        City newCity = new City(name, state);
+
+        if (cities.put(name, newCity) != null) {
+            throw new CrudException("City cannot be created.");
+        }
+
+        persist();
+    }
+
+    public Object[] read(final Object id) {
+        City city = cities.get((String) id);
 
         Object[] row = { city.getName(), city.getState() };
 
         return row;
     }
 
-    public static City getCity(final int index) {
-        return cities.get(index);
-    }
+    public void delete(final Object id) {
+        cities.remove((String) id);
 
-    public static ArrayList<City> getAll() {
-        return cities;
-    }
-
-    public static boolean delete(final int index) {
-        if (read(index) == null) {
-            return false;
-        }
-
-        cities.remove(index);
-        return true;
-    }
-
-    public static DefaultTableModel getTableModel() {
-        Object[] header = { "Name", "State" };
-
-        DefaultTableModel model = new DefaultTableModel(header, 0);
-
-        for (int i = 0; i < cities.size(); i++) {
-            model.addRow(new Object[] { cities.get(i).getName(), cities.get(i).getState() });
-        }
-
-        return model;
+        persist();
     }
 }
